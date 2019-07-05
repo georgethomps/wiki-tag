645b537b157ba922ef906f13d4ded99b64f57c75
George Thompson <ghthompson19@gmail.com>
2019-07-05 12:39:05 -0500

    Major overhaul to allow multiple album tagging

--- wikitag/albumtagger/album_tagger.py
+++ wikitag/albumtagger/album_tagger.py
@@ -1,61 +1,106 @@
 import os
+from collections import namedtuple
+from itertools import repeat
 
 from albumscraper.album_scraper import AlbumScraper
 from albumtagger.tagger_helpers.tagger_mixin import *
 
 
+# TODO: USE UNICODE OBJECTS!!!!!!!!!!!!!!!!!!!!!
 class AlbumTagger(TaggerMixin):
     """WARNING: For now this is solely designed for FLAC files, but more audio formats will be implemented through
-    the tag_map module in the tagger_helpers package."""
+    the tag_map module in the tagger_helpers package.
 
-    def __init__(self, album_path=None, album_url=None):
-        # handle the tagger's album path
-        if album_path:
-            self.open_album(path=album_path)
+    WARNING: It's also important to note that an AlbumTagger can only scrape album data once!
+
+    WARNING: Files in the directory must be sorted in the correct track order!"""
+
+    album = namedtuple('Album', ['path', 'url', 'data_map'])  # helps organize the albums' data
+
+    def __init__(self, album_paths=None, album_urls=None):
+        # Require users to provide an album path and album url when adding an album
+        if not album_paths and not album_urls:
+            self.albums = None
+
+        # handle strings
+        elif isinstance(album_paths, str) and isinstance(album_urls, str):
+            # TODO: what is this syntax?!
+            self.albums = []
+            self.albums.append(AlbumTagger.album(path=album_paths, url=album_urls, data_map='No Scraped Info'))
+            self.scrape_albums()
+
+        # handle sequences
+        elif len(album_paths) == len(album_urls):
+            self.albums = list(AlbumTagger.album(*items) for items in
+                               zip(album_paths, album_urls, repeat('No Scraped Info', len(album_paths))))
+            self.scrape_albums()
+
+        # unequal number of album paths and album urls
         else:
-            self._album_path = None
+            raise AlbumDataError
 
-        # handle scraping the album's info from the web
-        if album_url:
-            self.scrape_album_info(album_url=album_url)
-        else:
-            self._album_info = None
+    @property
+    def _album_paths(self):
+        return [album_path for album_path, album_url, album_map in self.albums]
 
-    def open_album(self, path):
-        """Sets the AlbumTagger's album path to a provided path and sets the path as the working directory."""
+    @property
+    def _album_urls(self):
+        return [album_url for album_path, album_url, album_map in self.albums]
+
+    def add_album(self, album_path, album_url):
         try:
-            self._album_path = path
-            os.chdir(path=path)
+            self.albums.append(AlbumTagger.album(path=album_path, url=album_url, data_map='No Scraped Info'))
+        except TypeError:
+            raise AlbumDataError
 
-        except TypeError as e:
+    @staticmethod
+    def open_album(album_path):
+        """Sets the AlbumTagger's album path to a provided path and sets the path as the working directory."""
+        # TODO: issue with relative paths!
+        try:
+            os.chdir(path=album_path)
+
+        except TypeError:
             raise PathTypeError
 
         except FileNotFoundError:
-            raise AlbumNotFoundError(path=path)
+            raise AlbumNotFoundError(path=album_path)
 
-    def scrape_album_info(self, album_url):
-        """Scrapes and stores the album's wikipedia info."""
-        scraper = AlbumScraper(url=album_url)
+    # TODO: processing to check if data was scraped
+    def scrape_albums(self):
+        """Scrapes and stores the album's Wikipedia info."""
+        urls = self._album_urls
+        # TODO: handle bad links!!!!!!!!
+        scraper = AlbumScraper(urls=urls)
         scraper.crawl()
-        self._album_info = scraper.album
+        self._update_album_info(scraper.albums)
 
-    # TODO: album property to display album info
+    def _update_album_info(self, info_maps):
+        """Recreate the albums attribute after scraping the albums' data (info map)"""
+        for index, album in enumerate(self.albums):
+            map_ = info_maps[index]
+            self.albums[index] = AlbumTagger.album(path=album.path, url=album.url, data_map=map_)
 
-    # TODO: decorator check if the album info was scraped
-    def tag_tracks(self, print_untagged=True):
-        """Iterates through the scraped album info and edits the tags of the album's files in-place."""
+    # TODO: albums property to display album info
+
+    def tag_albums(self, print_untagged=True):
+        for album in self.albums:
+            AlbumTagger.tag_album(album_path=album.path, album_map=album.data_map, print_untagged=print_untagged)
+
+    @staticmethod
+    def tag_album(album_path, album_map, print_untagged=True):
+        """Iterates through a dictionary of an album's scraped info and edits the tags of the album's files in-place."""
         # TODO: probably can use async to make this much more efficient!
 
         untagged = []  # save untagged files to report to the user
         # TODO: create a named tuple to provide reasons for why files/folders weren't tagged
 
-        album_info = self._album_info
-        album_data = {k: album_info[k] for k in album_info.keys() ^ {'tracks'}}
-        track_data = album_info['tracks']
+        AlbumTagger.open_album(album_path=album_path)
 
-        # TODO: only iterate over files if it is a flac file!!!!!!  duck typing
-        # TODO: WARNING: files in directory must be in the correct order!
-        files = sorted(os.listdir(path=self._album_path))
+        album_data = {k: album_map[k] for k in album_map.keys() ^ {'tracks'}}
+        track_data = album_map['tracks']
+
+        files = sorted(os.listdir(path=album_path))
         for index, file in enumerate(files):
             try:
                 mutagen_file = AlbumTagger.open_track(file_name=file)
@@ -83,13 +128,14 @@ class AlbumTagger(TaggerMixin):
             print()
 
 
-# TODO: USE UNICODE OBJECTS!!!!!!!!!!!!!!!!!!!!!
 if __name__ == '__main__':
     directory = '/home/george/projects/wiki-tag/wikitag/albumtagger/test_album'
-    tagger = AlbumTagger(album_path=directory,
-                         album_url='https://en.wikipedia.org/wiki/Rodeo_(Travis_Scott_album)')
+    dir2 = '/home/george/projects/wiki-tag/wikitag/albumtagger/test2'
+    tagger = AlbumTagger(album_paths=[directory, dir2],
+                         album_urls=['https://en.wikipedia.org/wiki/Rodeo_(Travis_Scott_album)',
+                                     'https://en.wikipedia.org/wiki/Rap_or_Go_to_the_League'])
 
-    tagger.tag_tracks()
+    tagger.tag_albums()
 
     # test scraping
     print('HERE IS ALBUM PRINT\n\n\n\n')
