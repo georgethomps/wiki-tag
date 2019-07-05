601e835e8e1af3f419aa45a995dfd847ab0f2882
George Thompson <ghthompson19@gmail.com>
2019-07-05 12:39:32 -0500

    Removed redundant code

--- wikitag/albumscraper/album_scraper.py
+++ wikitag/albumscraper/album_scraper.py
@@ -8,13 +8,12 @@ class AlbumScraper:
     makes it possible to internally collect data extracted from Scrapy as opposed to having to save the data to
     physical files!
 
-    WARNING: It's important to know that run() can only be used once due to the nature of the Scrapy framework!"""
+    WARNING: It's important to know that run() can only be used once due to the nature of the Scrapy framework!
+    Furthermore, only one AlbumScraper can be implemented in one's code."""
 
     # allows users to decided which urls to request
     def __init__(self, urls):
-        self._memory_link = []  # Takes advantage of call by sharing to retrieve parsed data.
-        self._albums = []
-        self._spider = AlbumsSpider(memory_link=self._memory_link)
+        self._albums = []  # Takes advantage of call by sharing to retrieve parsed data.
         self.urls = urls
 
     @property
@@ -23,34 +22,28 @@ class AlbumScraper:
 
     @property
     def urls(self):
-        return self.__dict__['_spider'].urls  # Just keep the URL stored in one place (inner class attribute)
+        return AlbumsSpider.urls  # Just keep the URL stored in one place (inner class attribute)
 
     @urls.setter
     def urls(self, urls):
-        """Simplifies the url-setting process"""
-
-        # this class was designed to only handle one album: no other sequences/iterables should be allowed
+        """Simplifies the url-setting process by properly appending sequences or strings"""
         if isinstance(urls, str):
-            self._spider.urls.append(urls)
+            AlbumsSpider.urls.append(urls)
         else:
-            self._spider.urls.extend(urls)
+            AlbumsSpider.urls.extend(urls)
 
-    # Store the scraper's album data and flush the memory link
-    def _flush_memory_link(self):
-        self._albums = self._memory_link[0]  # without a copy some serious bugs would occur!
-        self._memory_link.clear()
-
+    # TODO: needs more intelligible name
     def crawl(self):
         """Allow the Scraper's inner spider to crawl across Requests and store album data"""
         process = CrawlerProcess()  # no settings needed, they are defined in the Spider
-        process.crawl(self._spider, self._memory_link)
+        process.crawl(AlbumsSpider, self._albums)
         process.start()
-        self._flush_memory_link()  # store the album data in the Scraper's album attribute and flush the link.
 
 
 # TODO: need to make this be able to scrape multiple albums!!!!!!!!!!!
 if __name__ == '__main__':
-    album_url = 'https://en.wikipedia.org/wiki/Rodeo_(Travis_Scott_album)'
-    hm = AlbumScraper(url=album_url)
+    album_url = ['https://en.wikipedia.org/wiki/Rodeo_(Travis_Scott_album)', 'https://en.wikipedia.org/wiki/Rap_or_Go_to_the_League']
+    hm = AlbumScraper(urls=album_url)
+    wtf = hm.urls
     hm.crawl()
-    print(hm.albums)
+    print(hm.albums[1])
